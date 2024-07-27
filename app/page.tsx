f712e4c82743d658e569ab685e4e746f2ca6d42e
deibyrayo <email.deibyrayo@gmail.com>
2024-07-26 23:10:46 -0500

    [ADD] My approach section

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import RecentProjects from "@/components/RecentProjects";
 import { navItems } from "@/data";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
+import Approach from "@/components/Approach";
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
         <RecentProjects />
         <Clients />
         <Experience />
+        <Approach />
       </div>
     </main>
   );
